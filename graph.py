72bc009fb0fa65ccd82b9e1d6c623ae0066ad026
PabloPardo <ppardoga7@gmail.com>
2016-01-31 23:18:58 +0100

    Finish graph class

--- graph.py
+++ graph.py
@@ -20,18 +20,16 @@ class Edge(object):
 
 
 class Graph(object):
-    def __init__(self, vertices=(), edges=()):
+    def __init__(self, edges=()):
 
         self.vertices = {}
-        for v in vertices:
-            self.add_vertex(col=v[0], row=v[1], alt=v[2])
-
         self.edges = {}
         for e in edges:
             if '<Vertex cell=<Cell col=%r,row=%r>,alt=%r>' % (e[0][0], e[0][1], e[0][2]) not in self.vertices:
                 orig = Vertex(cell=Cell(col=e[0][0], row=e[0][1]), alt=e[0][2])
             else:
                 orig = self.get_vertex(col=e[0][0], row=e[0][1], alt=e[0][2])
+
             if '<Vertex cell=<Cell col=%r,row=%r>,alt=%r>' % (e[1][0], e[1][1], e[1][2]) not in self.vertices:
                 dest = Vertex(cell=Cell(col=e[1][0], row=e[1][1]), alt=e[1][2])
             else:
@@ -50,4 +48,10 @@ class Graph(object):
     def add_edge(self, orig, dest):
         e = Edge(orig=orig, dest=dest)
         self.edges[e.__repr__()] = e
+
+    def get_vertex(self, col, row, alt):
+        return self.vertices['<Vertex cell=<Cell col=%r,row=%r>,alt=%r>' % (col, row, alt)]
+
+    def get_edge(self, orig, dest):
+        return self.vertices['<Vertex orig=%r,dest=%r>' % (orig, dest)]
 
